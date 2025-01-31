3ef9085f5e99b60f1204891d0b81fae5e23de74e
TKMhub <111867865+TKMhub@users.noreply.github.com>
2025-01-31 23:59:22 +0900

    decrementing_serverOnly

--- src/lib/credits.ts
+++ src/lib/credits.ts
@@ -1,3 +1,4 @@
+import "server only";
 import { currentUser } from "@clerk/nextjs/server";
 import { prisma } from "./prisma";
 
@@ -17,8 +18,31 @@ export async function getUserCredits() {
         credits: true,
       },
     });
+    return dbUser?.credits ?? 0;
   } catch (error) {
     console.log("Error fetching user credits:", error);
     return 0;
+  }
+}
+export async function decrementUserCredits(clerkId: string) {
+  try {
+    const user = await prisma.user.update({
+      where: {
+        clerkId: clerkId,
+      },
+      data: {
+        credits: {
+          decrement: 1,
+        },
+      },
+      select: {
+        credits: true,
+      },
+    });
+    return user?.credits ?? 0;
+  } catch (error) {
+    console.log("Error decrementing user credits:", error);
+
+    return new Error("Failed to update credits");
   }
 }
