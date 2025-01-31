55d36bf40ee5a4f9af8b487ffec03043cf342886
TKMhub <111867865+TKMhub@users.noreply.github.com>
2025-02-01 01:08:22 +0900

    credits_check

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,37 +1,66 @@
 "use server";
 
+import { decrementUserCredits, getUserCredits } from "@/lib/credits";
 import { GenerateImageState, RemoveBackgroundState } from "@/types/actions";
+import { currentUser } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export async function generateImage(
   state: GenerateImageState, //利用なし
   formData: FormData
 ): Promise<GenerateImageState> {
-  const keyword = formData.get("keyword");
+  const user = await currentUser();
+  //ユーザーチャック
+  if (!user) {
+    throw new Error("認証が必要です。");
+  }
 
-  if (!keyword || typeof keyword !== "string") {
-    return {
-      status: "error",
-      error: "キーワードを入力してください。",
-    };
+  const credits = await getUserCredits();
+  //クレジットチェック
+  if (credits === null || credits < 1) {
+    //?以降についてはクレジット残高が足りない旨の情報を付属
+    redirect("/dashboard/plan?reason=insufficient_credits");
   }
   try {
-    const response = await fetch(`${process.env.BASE_URL}/api/generate-image`, {
-      method: "POST",
-      headers: {
-        "Content-type": "application",
-      },
-      body: JSON.stringify({ keyword }),
-    });
+    const keyword = formData.get("keyword");
 
-    const data = await response.json();
+    if (!keyword || typeof keyword !== "string") {
+      return {
+        status: "error",
+        error: "キーワードを入力してください。",
+      };
+    }
+    try {
+      const response = await fetch(
+        `${process.env.BASE_URL}/api/generate-image`,
+        {
+          method: "POST",
+          headers: {
+            "Content-type": "application",
+          },
+          body: JSON.stringify({ keyword }),
+        }
+      );
 
-    console.log(data.imageUrl);
+      const data = await response.json();
 
-    return {
-      status: "success",
-      imageUrl: data.imageUrl,
-      keyword: keyword,
-    };
+      //利用時にクレジットをg減少させる
+      await decrementUserCredits(user.id);
+      revalidatePath("/dashboard");
+
+      return {
+        status: "success",
+        imageUrl: data.imageUrl,
+        keyword: keyword,
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        status: "error",
+        error: "画像の生成に失敗しました。",
+      };
+    }
   } catch (error) {
     console.log(error);
     return {
