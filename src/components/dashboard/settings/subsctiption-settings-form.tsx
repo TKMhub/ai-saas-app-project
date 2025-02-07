33068da9724a1eb801d0072d6a7195049eb6badc
TKMhub <111867865+TKMhub@users.noreply.github.com>
2025-02-07 23:53:34 +0900

    settinf_ui

--- src/components/dashboard/settings/subsctiption-settings-form.tsx
+++ src/components/dashboard/settings/subsctiption-settings-form.tsx
@@ -1,5 +1,32 @@
-const SubscriptionSettingsForm = () => {
-  return <div>SubscriptionSettingsForm</div>;
+"use client";
+import { Button } from "@/components/ui/button";
+import { User } from "@prisma/client";
+
+interface SettingsFormProps {
+  user: User;
+}
+
+const SubscriptionSettingsForm = ({ user }: SettingsFormProps) => {
+  const handleManageSubscription = async () => {};
+
+  return (
+    <div>
+      <div>
+        {user?.subscriptionStatus !== "FREE" ? (
+          <>
+            <p>現在のサブスクリプションを管理します。</p>
+            <Button onClick={handleManageSubscription}>
+              サブスクリプション管理
+            </Button>
+          </>
+        ) : (
+          <>
+            <p>サブスクリプションに登録していません。</p>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default SubscriptionSettingsForm;
