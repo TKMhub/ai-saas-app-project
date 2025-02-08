3f6e25db7f752e93246623c0d7c20bc9d5033602
TKMhub <111867865+TKMhub@users.noreply.github.com>
2025-02-08 16:32:54 +0900

    fetching

--- src/components/dashboard/settings/subsctiption-settings-form.tsx
+++ src/components/dashboard/settings/subsctiption-settings-form.tsx
@@ -1,13 +1,26 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { User } from "@prisma/client";
+import { useRouter } from "next/navigation";
 
 interface SettingsFormProps {
   user: User;
 }
 
 const SubscriptionSettingsForm = ({ user }: SettingsFormProps) => {
-  const handleManageSubscription = async () => {};
+  const router = useRouter();
+  const handleManageSubscription = async () => {
+    try {
+      const response = await fetch("/api/create-portal-session", {
+        method: "POST",
+      });
+
+      const data = await response.json();
+      router.push(data.url);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
   return (
     <div className="grid gap-4 p-4 border-2 rounded-lg">
